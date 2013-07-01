acae8cd577297811f38a23467ebeee52ef2e4c05
kleinsimon <sklein8@googlemail.com>
2013-07-01 21:35:34 +0200

    Move

--- PressureTool/Helper.cs
+++ PressureTool/Helper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Windows.Forms;
 
 namespace PressureTool
 {
@@ -196,6 +197,33 @@ namespace PressureTool
             logFileWriter.Flush();
             logFileWriter.Close();
             logFileWriter.Dispose();
+        }
+    }
+
+    public static class SuspendUpdate
+    {
+        private const int WM_SETREDRAW = 0x000B;
+
+        public static void Suspend(Control control)
+        {
+            Message msgSuspendUpdate = Message.Create(control.Handle, WM_SETREDRAW, IntPtr.Zero,
+                IntPtr.Zero);
+
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgSuspendUpdate);
+        }
+
+        public static void Resume(Control control)
+        {
+            // Create a C "true" boolean as an IntPtr
+            IntPtr wparam = new IntPtr(1);
+            Message msgResumeUpdate = Message.Create(control.Handle, WM_SETREDRAW, wparam,
+                IntPtr.Zero);
+
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgResumeUpdate);
+
+            control.Invalidate();
         }
     }
 }
